8a8bfb69ab2ac089f17102469e6b0666f239ea29
al mubarok <daffa1mubarok@gmail.com>
2023-07-21 16:07:18 +0700

    menambahkan app baru (users)

--- sql/airline/flights/admin.py
+++ sql/airline/flights/admin.py
@@ -3,7 +3,16 @@ from .models import Flight, Airport, Passenger
 
 # ini untuk section admin, jadi hanya admin yang bisa mengakses section ini nanti
 
+class FlightAdmin(admin.ModelAdmin) :
+    list_display = ("id", "origin", "destination", "duration")
+    # nama variable sangat diperhatikan, harus "list_display"
+    # class ini digunakan untuk menampilkan tabel halaman admin
+
+class Passengeradmin(admin.ModelAdmin) :
+    filter_horizontal = ("flights",)
+    # filter_horizontal harus berisi tuple, dengan "," di akhirnya
+
 # Register your models here.
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, Passengeradmin)
--- sql/airline/flights/views.py
+++ sql/airline/flights/views.py
@@ -16,12 +16,16 @@ def flight(request, flight_id) :
         "flight" : flight,
         "passengers" : flight.passengers.all(),
         "non_passengers" : Passenger.objects.exclude(flights=flight).all()
+        # non_passenger menyimpan data Passenger yang tidak terdata pada variable flight pada func ini
     })
 
 def book(request, flight_id) :
     if request.method == "POST" :
         flight = Flight.objects.get(pk=flight_id)
         passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        # passenger mengambil data Passenger dari pk yang kita POST pada HTML
         passenger.flights.add(flight)
+        # bagian ini untuk menambahkan flight ke dalam passenger pada func ini
         return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
+        # setelah semua tereksekusi, maka kita akan kembali ke halaman "flight" dengan args flight_id
 
